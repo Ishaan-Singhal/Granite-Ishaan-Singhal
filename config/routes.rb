8c8fbe0c31a634b43eac29b3b0cd45b4f9e6e663
Ishaan Singhal <ishaan.singhal@bigbinary.com>
2022-09-02 23:12:31 +0530

    Added ability to delete a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tasks, except: %i[new edit destroy], param: :slug
+  resources :tasks, except: %i[new edit], param: :slug
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
   get "*path", to: "home#index", via: :all
