fa4cab2e725ddcec3d9ee9763e2059e36da999e4
Ishaan Singhal <ishaan.singhal@bigbinary.com>
2022-09-06 11:39:25 +0530

    Added task owner to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,7 +6,7 @@ class Task < ApplicationRecord
   validate :slug_not_changed
 
   before_create :set_slug
-
+  belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
   belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
 
   private
